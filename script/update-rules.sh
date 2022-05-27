6e1b9d28f86574c2ef053072dcda8b4be0ae70d1
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-05-27 23:50:59 +0800

    add quicklist

--- script/update-rules.sh
+++ script/update-rules.sh
@@ -84,6 +84,7 @@ dns=(
   "https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt"
   "https://raw.githubusercontent.com/hacamer/AdRule/main/dns.txt"
    "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt"
+   "https://fastly.jsdelivr.net/gh/francis-zhao/quarklist/dist/quarklist.txt"
 #  "https://blocklistproject.github.io/Lists/adguard/ads-ags.txt"
 #  "https://blocklistproject.github.io/Lists/adguard/tracking-ags.txt"
 #  "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/adblock/adblock.txt"
