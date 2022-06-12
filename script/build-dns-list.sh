c209fcddd12086d0cd05d88b72b0b5437ee980f2
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-12 11:56:27 +0800

    逻辑处理

--- script/build-dns-list.sh
+++ script/build-dns-list.sh
@@ -16,6 +16,10 @@ wait
 php ./tools/easylist-extend.php ../dns.txt &
 cd ../
 cat ./tmp/l.txt >> dns.txt
+
+cat ./tmp/dns998* >>../dns.txt
+cat ./mod/rules/*-rules.txt |grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" |sort|uniq >> ../dns.txt
+
 cat ./script/*/white_domain_list.php |grep -Po "(?<=').+(?=')" |sed "s/^/||&/g" |sed "s/$/&^/g"| sed '/^$/d'   > allowtest.txt
 hostlist-compiler -c ./script/dns-rules-config.json -o dns-output.txt &
 wait
