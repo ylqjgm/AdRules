38c1388d98c614e0f3d9ee356f9ea470312e1476
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-13 06:59:09 +0800

    Update build-dns-list.sh

--- script/build-dns-list.sh
+++ script/build-dns-list.sh
@@ -20,8 +20,8 @@ cat ./tmp/l.txt >> dns.txt
 cat ./tmp/dns998* >>../dns.txt
 cat ./mod/rules/*-rules.txt |grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" |sort|uniq >> ../dns.txt
 
-cat ./script/*/white_domain_list.php |grep -Po "(?<=').+(?=')" |sed "s/^/||&/g" |sed "s/$/&^/g"| sed '/^$/d'   > allowtest.txt
-cat ./script/*/white_domain_list.php |grep -Po "(?<=').+(?=')" |sed "s/^/||&/g" |sed "s/$/&^/g"| sed '/^$/d'   >> allowtest.txt
+cat ./script/*/white_domain_list.php |grep -Po "(?<=').+(?=')" | sed '/^$/d'   > allowtest.txt
+cat ./script/*/white_domain_list.php |grep -Po "(?<=').+(?=')" | sed '/^$/d'   >> allowtest.txt
 hostlist-compiler -c ./script/dns-rules-config.json -o dns-output.txt &
 wait
 rm -f allowtest.txt
