a4ac8b0c5afbebdd1f396626b3cd698228eb93de
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-13 06:52:03 +0800

    Update build-dns-list.sh

--- script/build-dns-list.sh
+++ script/build-dns-list.sh
@@ -23,6 +23,7 @@ cat ./mod/rules/*-rules.txt |grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" |sort|uniq >>
 cat ./script/*/white_domain_list.php |grep -Po "(?<=').+(?=')" |sed "s/^/||&/g" |sed "s/$/&^/g"| sed '/^$/d'   > allowtest.txt
 hostlist-compiler -c ./script/dns-rules-config.json -o dns-output.txt &
 wait
+rm -f allowtest.txt
 mv -f dns-output.txt dns.txt
 cd ./script/
 #sed -i "s/\|\|tanx.com\^/\|\|tanx.com\^\$dnstype\=\~CNAME/" ../dns.txt
