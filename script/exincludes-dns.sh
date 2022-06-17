4a98353d88dbc59f273a3ec081788113a7d72756
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-17 22:51:32 +0800

    Add RM

--- script/exincludes-dns.sh
+++ script/exincludes-dns.sh
@@ -2,7 +2,15 @@
 cname=(
  "tanx.com"
 )
+rm=(
+ "windows.net"
+)
 for i in $cname
+do
+  sed -i "s/||$i^/||$i^\$dnstype=~CNAME/" dns.txt
+done
+
+for i in $rm
 do
   sed -i "s/||$i^/||$i^\$dnstype=~CNAME/" dns.txt
 done
