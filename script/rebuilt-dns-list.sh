74c6ed9986dffd645d752c5d5768f3eeb4e4798d
hacamer <hcamer145@gmail.com>
2022-06-19 21:33:51 +0800

    Test

--- script/rebuilt-dns-list.sh
+++ script/rebuilt-dns-list.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 cd ./origin-files
+echo 开始处理DNS规则
 yc=`cat dns* hosts*| grep -vE ']|@|!' |grep -v -E "^((#.*)|(\s*))$"  | grep -v -E "^[0-9\.:]+\s+(ip6\-)?(localhost|loopback)$"  | sed -e "s/||//g" -e "s/\^//g" -e "s/0.0.0.0 //g" -e "s/127.0.0.1 //g" | sed 's/[ ]//g'|sort|uniq `
-wl=`cat allow-lists.txt |sed "s/\#.*//g"`
+wl=`cat allowlists.txt |sed "s/\#.*//g"`
 dead=`cat base-dead-hosts.txt |sed "s/\#.*//g"`
 wl0=`printf "%s\n" "$wl" |grep '^0 '`
 wl1=`printf "%s\n" "$wl" |grep '^1 '`
+
+cat pre-rules.txt cat base-dead-hosts.txt cat base-dead-hosts.txt |sort |uniq -u > 1.txt
+mv -f 1.txt pre-rules.txt
 printf "%s\n" "$yc" >pre-rules.txt
 for i in $wl0
-do
-  sed -i 's/$i//g' pre-rules.txt &
-done
-wait 
-for i in $dead
 do
   sed -i 's/$i//g' pre-rules.txt &
 done
