ba8f3bb75d47c7614f68c09e552ef0279b6fe9f2
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-20 11:08:59 +0800

    fixc

--- script/rebuilt-dns-list.sh
+++ script/rebuilt-dns-list.sh
@@ -21,10 +21,10 @@ do
   sed -i 's/.*$i//g' pre-rules.txt &
 done
 wait
-i=`cat pre-rules.txt|grep -v "^$"|sed 's/.*#.*//g' `
-echo "$i" >> ../../ad-domains.txt
-echo "$i" >> ../../dns.txt
-echo "$i" |sed 's/^/0.0.0.0 /g' >> ../../hosts.txt
+i=`cat pre-rules.txt|grep -v "#"|sed 's/.*#.*//g' | sed '/^$/d'`
+echo "$i"| sed '/^$/d' >> ../../ad-domains.txt
+echo "$i"| sed '/^$/d' >> ../../dns.txt
+echo "$i"| sed '/^$/d' |sed 's/^/0.0.0.0 /g' >> ../../hosts.txt
 wait
 cd ../../
 cat ./tmp/l.txt >> dns.txt
