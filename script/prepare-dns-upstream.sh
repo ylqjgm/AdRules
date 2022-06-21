e745cf3b4e44ce49acd2714384355f2bec8e27b1
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-06-21 17:21:39 +0800

    Update prepare-dns-upstream.sh

--- script/prepare-dns-upstream.sh
+++ script/prepare-dns-upstream.sh
@@ -28,7 +28,7 @@ cd origin-files
 mv pre-allow1.txt dns99999.txt
 cat hosts*.txt | grep -v -E "^((#.*)|(\s*)|(\!.*))$" \
  | grep -v -E "^[0-9\.:]+\s+(ip6\-)?(localhost|loopback)$" \
- | sed s/0.0.0.0/127.0.0.1/g | sed s/::/127.0.0.1/g | sort \
+ | sed 's/0.0.0.0/127.0.0.1/g' | sed 's/::/127.0.0.1/g' | sort \
  | uniq >base-src-hosts.txt
 
 cat dead-hosts*.txt | grep -v -E "^(#|\!)" \
